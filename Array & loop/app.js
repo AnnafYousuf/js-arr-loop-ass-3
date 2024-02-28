2f903a5490a59a684710e3bf3851a111bedb53df
AnnafYousuf <annafyousuf1@gmail.com>
2024-02-28 13:20:45 +0500

    Assignment Done

---
+++ Array & loop/app.js
@@ -0,0 +1,44 @@
+// let tvalue = +prompt("Enter Table Value")
+// let tsvalue = +prompt("Enter Table Starting Value")
+// let tevalue = +prompt("Enter Table End Value")
+
+// for(let i = tsvalue; i<= tevalue;i++ ){
+//     document.write(tvalue + "x" + i + " = " + tvalue*i + "<br>")
+// }
+
+
+
+// let students = ["Annaf", "Asad", "Bilal", 123, 659, 987]
+
+// students.push("Asjad", "Babar")
+// students.pop()
+// students.pop()
+// students.pop()
+// students.unshift("Naeem", "Waseem")
+// students.shift()
+// students.shift()
+// students.shift()
+// students.splice(2,5, "Akbar", "Akmal")
+// let arr = students.slice(1,3)
+
+// console.log(students);
+// console.log(arr);
+
+
+
+
+// let arr = ["Annaf", "Asad", "Bilal", 123, 659, 987]
+// let arr1 = ["Annaf", 123, 659, 987, "Ubaid", "Lateef"]
+
+// let result = arr.indexOf("Bilal")
+// let result = arr.includes("Annaf")
+// let result = arr.includes("Ali")
+// let result = arr.concat(arr1)
+
+// console.log(result);
+
+
+
+// let num = [9,2,3,5,7,1,8,6]
+// let result = num.sort()
+// console.log(result);
